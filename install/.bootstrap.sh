a88c5107d9dc713ac652bcd886cfa85e62eb5a2a
Charlie <charles.boswell@ukaea.uk>
2022-07-20 12:36:47 +0100

    mux3 completed

--- install/.bootstrap.sh
+++ install/.bootstrap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 apt update
-apt-get install -y vim
 apt-get install -y build-essential
+apt-get install -y vim
+apt-get install -y tmux
 apt-get install -y make
 apt-get install -y cmake
 apt-get install -y git
