a92f66606ecdbfe881ec2e9e0cd33803e1363f94
Charlie <charles.boswell@ukaea.uk>
2022-09-14 15:48:05 +0100

    tcpreplay installed

--- install/.bootstrap.sh
+++ install/.bootstrap.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 apt update
-apt-get install -y build-essential
+apt-get install -y build-essential libcap-dev
 apt-get install -y vim
 apt-get install -y tmux
 apt-get install -y make
 apt-get install -y cmake
 apt-get install -y git
+apt-get install -y autotools-dev
+apt-get install -y libtool
+apt-get install -y automake
+apt-get install -y autogen
+apt-get install -y libpcap-dev
+git clone https://github.com/appneta/tcpreplay.git
+cd tcpreplay
+./autogen
+./configure --disable-local-libopts
+make
+make install
+cd ../
 git clone https://github.com/cboswel/MARTe-demos.git
