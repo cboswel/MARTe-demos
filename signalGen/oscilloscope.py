234f13edfb70b60074ca7958a8f4980e380019d3
Charlie <charles.boswell@ukaea.uk>
2022-10-06 16:00:50 +0100

    better waves, inaccurate axes

--- signalGen/oscilloscope.py
+++ signalGen/oscilloscope.py
@@ -3,7 +3,7 @@
 import matplotlib.pyplot as plt
 import subprocess
 
-output = subprocess.getoutput('head -n -1 wave.csv | tail -n -100')
+output = subprocess.getoutput('head -n -1 wave.csv | tail -n -200')
 Y = output.splitlines()
 
 plt.ion()
@@ -13,8 +13,9 @@ plt.ylabel('amplitude')
 
 while True:
 
-    output = subprocess.getoutput('head -n -1 wave.csv | tail -n -100')
+    output = subprocess.getoutput('head -n -1 wave.csv | tail -n -200')
     Y = output.splitlines()
+    Y = [float(line) for line in Y]
     graph.set_ydata(Y)
     plt.draw()
     plt.pause(0.01)
